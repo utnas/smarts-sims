55a4502ad8820941f818688fc0aee82680ab6d0f
utnas <nsewolo@gmail.com>
2012-12-08 10:01:13 +0100

    debug

--- config/routes.rb
+++ config/routes.rb
@@ -79,6 +79,8 @@ Sims::Application.routes.draw do
   # just remember to delete public/index.html.save.
   root :to => 'inhabitants#index'
 
+  break if ARGV.join.include? 'assets:precompile'
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
