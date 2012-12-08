f0fc76ca6d298f3e3b9c807c5da050f93e28aafa
utnas <nsewolo@gmail.com>
2012-12-08 09:47:15 +0100

    error page not exists- debug

--- db/migrate/20121205220454_create_users.rb
+++ db/migrate/20121205220454_create_users.rb
@@ -1,14 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
   	#drop_table :users
-    create_table :users do |t|
-      t.string :username
-      t.string :email
-      t.string :crypted_password
-      t.string :salt
-      t.boolean :admin, :default => false
+    # create_table :users do |t|
+    #   t.string :username
+    #   t.string :email
+    #   t.string :crypted_password
+    #   t.string :salt
+    #   t.boolean :admin, :default => false
 
-      t.timestamps
+    #   t.timestamps
     end
   end
 end
