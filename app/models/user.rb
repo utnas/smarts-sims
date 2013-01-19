443c77c3f6d3e40c218711c17968e84f9c9a60e4
utnas <nsewolo@gmail.com>
2013-01-19 23:16:20 +0100

    password length increased to 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   attr_accessible :username, :email, :password, :password_confirmation, :admin
 
-  validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
+  validates_length_of :password, :minimum => 8, :message => "password must be at least 3 characters long", :if => :password
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
   validates_presence_of :password, :email, :username
   validates_uniqueness_of :email, :username
