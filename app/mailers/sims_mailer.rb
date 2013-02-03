72a1cedd3c3390fff44316979b0120edbfb80c1e
utnas <nsewolo@gmail.com>
2013-02-03 17:30:13 +0100

    issue resolving, mailer

--- app/mailers/sims_mailer.rb
+++ app/mailers/sims_mailer.rb
@@ -4,12 +4,12 @@ class SimsMailer < ActionMailer::Base
 		@user = user
 
 	    @recipients = [user.email]
-	    @cci = ['nsewolo@yahoo.fr']
+	    @cc = ['nsewolo@yahoo.fr']
 	    @from = 'eddie.jaffuel@smartesting.com'
 	    @subject = 'Creation of account'
 	    @sent_on = Time.now
 	    @headers = {'Account creation' => 'Creation of your requested account'}
-	    mail(:from => @from, :to => @recipients, :subject => @subject, :cci => @cci)
+	    mail(:from => @from, :to => @recipients, :subject => @subject, :cc => @cc)
 	end
 
 end
