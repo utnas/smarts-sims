03b2ed1d331b9e7edc6210b17f98a136164347b1
Dooley <nsewolo@gmail.com>
2015-11-26 22:51:08 -0500

    upgrade completed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,1 @@
-
-User.create([{username: 'eddie',email: 'eddie.jaffuel@gmail.com',password: 'lti18lti18',admin: true}])
-User.create([{username: 'ed',email: 'edd@gmail.com',password: 'lti18lti18',admin: true}])
-User.create([{username: 'sama',email: 'sama@gmail.com',password: 'lti18lti18',admin: false}])
+User.create([{username: 'eddie', email: 'eddie.jaffuel@gmail.com', password: 'lti18lti18', admin: true}])
